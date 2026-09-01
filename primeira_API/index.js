1c4fae5782a1b78fb1494f68620ad4234a74a162
enzo-savaris <savarisenzo2@gmail.com>
2026-09-01 20:09:05 -0300

    aula dia 01/09/2026

--- primeira_API/index.js
+++ primeira_API/index.js
@@ -1,36 +1,61 @@
-const express = require('express');
+const express = require("express");
+
 const app = new express();
 
 app.use(express.json());
 
-let data = [{
-    nome   : "Enzo",
-    cpf    : "123.456.789-00",
-    status : true
+var contador_id = 1;
+var data = [{
+    id: 1,
+    nome: "ENzo Savaris",
+    cpf: "46999162280",
+    status: true
 }];
 
 app.get("/listar", (req, res) => {
     return res.send(data);
 });
 
+app.get("/listar/:id", (req, res) => {
+    const { id } = req.params;
+
+    const pessoa = data.filter((item) =>{
+        return item.id == id;
+    })
+
+    if (pessoa.length <= 0 || pessoa == undefined) {
+        msg = `Pessoa do código: ${id} não encontrada!`
+        return res.status(400).send(msg);
+    }
+    res.send(pessoa);
+});
+
 app.post("/cadastrar", (req, res) => {
+   
     const { nome, cpf, status } = req.body;
 
-    // const nome   = req.body.nome;
-    // const cpf    = req.body.cpf;
-    // const status = req.body.status;
+    if (!cpf) {
+        return res.send("O campo CPF é obrigatorio!");
+    }
+
+    contador_id++
+
+    data.push({
+        id: contador_id,
+        nome,
+        cpf,
+        status
+    });
 
     
-    data.push({ id: data.length + 1,
-                nome, 
-                cpf, 
-                status 
-            });
-    
+
     return res.send(data);
 });
 
 app.listen(8080, () => {
-  console.log('Servidor rodando na porta 8080');
-
+    console.log("O sevidor está rodando na porta 8080!");
 });
+
+
+//STATUS 500 - ERRO INTERNO, SEM EXPLICAÇÃO, IGUAL ELEITOR DO LULA(NÃO TEM EXPLICAÇÃO, SÓ EXISTE)
+//STATUS 400 - ERRO DE REQUISIÇÃO, FALTA ALGUM DADO.
