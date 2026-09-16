6fd547d5d7d042a16b8ca7fc38eb37409e2ef05b
enzo-savaris <savarisenzo2@gmail.com>
2026-09-16 19:14:38 -0300

    tabalho aula 16/09/2026

--- biblioteca/index.js
+++ biblioteca/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import knex from "knex";
 
 const app = new express();
+
 const mySql = knex({
     client: "mysql2",
     connection: {
@@ -12,11 +13,10 @@ const mySql = knex({
     }
 });
 
-
 async function testaConexaoComBancoDeDados() {
     try {
         await mySql.raw("SELECT 0 AS RESULT");
-        console.log("Sucesso ao conecatar ao banco de dados!");
+        console.log("Sucesso ao conectar ao banco de dados!");
     } catch (error) {
         console.log("Erro ao realizar conexão com banco de dados!");
     }
@@ -27,50 +27,81 @@ testaConexaoComBancoDeDados();
 app.use(express.json());
 
 app.listen(8080, () => {
-    console.log("O sevidor está rodando na porta 8080!");
+    console.log("O servidor está rodando na porta 8080!");
 });
 
 app.get("/listar", async (req, res) => {
-    const livros = await mySql.raw(" SELECT * FROM livros");     
 
-    res.send(livros[0]);
-})
-
-app.post("/cadastrar", async (req, res)=> {
-    const {titulo, autor,editora, qtde, preco} = req.body;
-    const livros = await mySql.raw("INSERT INTO livros(TITULO, AUTOR, EDITORA, QTDE, PRECO) VALUES(?,?,?,?,?)", [titulo, autor, editora, qtde, preco]);
-   
-    var msg = "Livro cadastrado com sucesso!";
-    res.send(msg);
-}
-);
-
-app.put("/atualizar/:id", async (req, res) => {
-  const { id } = req.params;
-  const { titulo, autor, editora, qtde, preco } = req.body;
-
-  await mySql.raw(
-    `UPDATE livros
-     SET titulo = ?,
-         autor = ?,
-         editora = ?,
-         qtde = ?,
-         preco = ?
-     WHERE id = ?`,
-    [titulo, autor, editora, qtde, preco, id]
-  );
-
-  res.send("Livro atualizado com sucesso!");
+    try {
+        const livros = await mySql.raw("SELECT * FROM livros");
+        res.status(200).send(livros[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Erro ao listar os livros!");
+    }
 });
 
+app.post("/cadastrar", async (req, res) => {
+    try {
+        const { titulo, autor, editora, qtde, preco } = req.body;
+        await mySql.raw(
+            `INSERT INTO livros
+            (TITULO, AUTOR, EDITORA, QTDE, PRECO)
+            VALUES (?, ?, ?, ?, ?)`,
+            [titulo, autor, editora, qtde, preco]
+        );
+        res.status(201).send("Livro cadastrado com sucesso!");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Erro ao cadastrar o livro!");
+    }
+});
+
+app.put("/atualizar/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { titulo, autor, editora, qtde, preco } = req.body;
+        await mySql.raw(
+            `UPDATE livros
+             SET TITULO = ?,
+                 AUTOR = ?,
+                 EDITORA = ?,
+                 QTDE = ?,
+                 PRECO = ?
+             WHERE ID = ?`,
+            [titulo, autor, editora, qtde, preco, id]
+        );
+        res.status(200).send("Livro atualizado com sucesso!");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Erro ao atualizar o livro!");
+    }
+
+});
 app.delete("/deletar/:id", async (req, res) => {
-    const { id } = req.params;
-    await mySql.raw("DELETE FROM livros WHERE id = ?", [id]);  
-    res.send("Livro deletado com sucesso!");
-})
+    try {
+        const { id } = req.params;
+        await mySql.raw(
+            "DELETE FROM livros WHERE ID = ?",
+            [id]
+        );
+        res.status(200).send("Livro deletado com sucesso!");
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Erro ao deletar o livro!");
+    }
+});
 
 app.get("/listar/:id", async (req, res) => {
-    const { id } = req.params;
-    const livros = await mySql.raw(" SELECT * FROM livros WHERE ID = ?", [id]);
-    res.send(livros[0]);
+    try {
+        const { id } = req.params;
+        const livros = await mySql.raw(
+            "SELECT * FROM livros WHERE ID = ?",
+            [id]
+        );
+        res.status(200).send(livros[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Erro ao buscar o livro!");
+    }
 });
